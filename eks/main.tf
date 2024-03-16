3d1b3caf60e092309188d946303edf597f108124
bbru205 <kumarkannabaddi@gmail.com>
2024-03-16 15:11:30 +0530

    jj

---
+++ eks/main.tf
@@ -0,0 +1,311 @@
+variable "aws_region" {
+  description = "The AWS region"
+  default     = "us-east-1"
+}
+
+variable "aws_access_key" {
+  description = "The AWS access key"
+   default     = "AKIA5LPT3OPDAFBYCB6J"
+}
+
+variable "aws_secret_key" {
+  description = "The AWS secret key"
+   default     = "PZ8y0bXP5Cn6/joYPx+iEWuXUOb+zDRHcGkpIPRw"
+}
+provider "aws" {
+
+  region = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+resource "aws_key_pair" "my-keys" {
+  key_name = "keys"
+public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDaTBy3LpvytVxV1/1Kg7tGERNBGeKJrUQPPE1ov4DK6+7zDnNUxJVPWoQ04hqonXjTRr5zrnV5WC9IrF8lk8X3wXrhxuHFAs18HXNz+iALgBjlDZedMjbUb/Z+JS1J2yyN8gUxxKMa28arNgkamVfEYcEL9yxM6s4Nqph2QX2gxOLKBfq83bhvgFA6h5g3xUjO8zeoRzJLAmp81FGQNwRbNDpH5S/bA7nIHBo53adbObXYt8PofhU6yR2PamGGwZPspksx8Kr9VZPVQiyQk/YrbuK1eRD3xCFIQAMjUgWM8FqNRm2AQEbsLOj8O4XYBdU5+8SvMc7ISplnxBtKIHnoKn4MtArcI+C09e2WvPzh3P6hg/Ii9c+1V5B1D1X6jY9J2SSbe6gGTMBrrd7/cQTZMNYJTxwpGrJ1LdXxGVzUj6BcA0U+HOsqSpmNYHl9GJbFdli8v2MdeQ1w3BBzSCES8x5eNWPbn6D08JX69cSF5To5EfzGXdiCIYnW9JaYVn0= prasanth@DESKTOP-RUR6DG9"
+}
+resource "aws_instance" "kubectl-server" {
+  ami                         = "ami-07d9b9ddc6cd8dd30"
+  key_name                    = "keys"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.public-1.id
+  vpc_security_group_ids      = [aws_security_group.allow_tls.id]
+
+  tags = {
+    Name = "kubectl"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.29.0/2024-01-04/bin/linux/amd64/kubectl",
+      "chmod +x ./kubectl",
+      "mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH",
+      "sudo snap install aws-cli --classic",
+      "aws configure set aws_access_key_id ${var.aws_access_key}",
+      "aws configure set aws_secret_access_key ${var.aws_secret_key}",
+      "aws configure set default.region ${var.aws_region}",
+      "aws configure set default.output json",
+      "aws eks update-kubeconfig --region ${var.aws_region} --name pc-eks",
+      "echo '--Added new context arn:aws:eks:${var.aws_region}:918023795654:cluster/pc-eks to $HOME/.kube/config'"
+    ]
+    connection {
+      type        = "ssh"
+      user        = "ubuntu" # or the appropriate user for your AMI
+      private_key = file("./keys")
+      host        = self.public_ip
+    }
+  }
+}
+
+resource "aws_eks_node_group" "node-grp" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "pc-node-group"
+  node_role_arn   = aws_iam_role.worker.arn
+  subnet_ids      = [aws_subnet.public-1.id, aws_subnet.public-2.id]
+  capacity_type   = "ON_DEMAND"
+  disk_size       = "20"
+  instance_types  = ["t2.small"]
+
+  remote_access {
+    ec2_ssh_key               = "keys"
+    source_security_group_ids = [aws_security_group.allow_tls.id]
+  }
+
+  labels = tomap({ env = "dev" })
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+    #aws_subnet.pub_sub1,
+    #aws_subnet.pub_sub2,
+  ]
+}
+resource "aws_iam_role" "master" {
+  name = "ed-eks-master"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.master.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.master.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.master.name
+}
+
+resource "aws_iam_role" "worker" {
+  name = "ed-eks-worker"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_policy" "autoscaler" {
+  name   = "ed-eks-autoscaler-policy"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "autoscaling:DescribeAutoScalingGroups",
+        "autoscaling:DescribeAutoScalingInstances",
+        "autoscaling:DescribeTags",
+        "autoscaling:DescribeLaunchConfigurations",
+        "autoscaling:SetDesiredCapacity",
+        "autoscaling:TerminateInstanceInAutoScalingGroup",
+        "ec2:DescribeLaunchTemplateVersions"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.worker.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.worker.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonSSMManagedInstanceCore" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  role       = aws_iam_role.worker.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.worker.name
+}
+
+resource "aws_iam_role_policy_attachment" "x-ray" {
+  policy_arn = "arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess"
+  role       = aws_iam_role.worker.name
+}
+resource "aws_iam_role_policy_attachment" "s3" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+  role       = aws_iam_role.worker.name
+}
+
+resource "aws_iam_role_policy_attachment" "autoscaler" {
+  policy_arn = aws_iam_policy.autoscaler.arn
+  role       = aws_iam_role.worker.name
+}
+
+resource "aws_iam_instance_profile" "worker" {
+  depends_on = [aws_iam_role.worker]
+  name       = "ed-eks-worker-new-profile1"
+  role       = aws_iam_role.worker.name
+}
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "PC-VPC"
+  }
+}
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "igw"
+  }
+}
+resource "aws_route_table" "rtb" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "MyRoute"
+  }
+}
+
+resource "aws_route_table_association" "a-1" {
+  subnet_id      = aws_subnet.public-1.id
+  route_table_id = aws_route_table.rtb.id
+}
+
+resource "aws_route_table_association" "a-2" {
+  subnet_id      = aws_subnet.public-2.id
+  route_table_id = aws_route_table.rtb.id
+}
+resource "aws_eks_cluster" "eks" {
+  name     = "pc-eks"
+  role_arn = aws_iam_role.master.arn
+
+
+  vpc_config {
+    subnet_ids = [aws_subnet.public-1.id, aws_subnet.public-2.id]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.AmazonEKSServicePolicy,
+    aws_iam_role_policy_attachment.AmazonEKSVPCResourceController,
+    aws_iam_role_policy_attachment.AmazonEKSVPCResourceController,
+    #aws_subnet.pub_sub1,
+    #aws_subnet.pub_sub2,
+  ]
+
+}
+
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+resource "aws_subnet" "public-1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-sub-1"
+  }
+}
+
+resource "aws_subnet" "public-2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-sub-2"
+  }
+}
